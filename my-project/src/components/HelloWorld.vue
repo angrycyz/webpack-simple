41dd6e97378ba271aa5323239a17a4b4ae61442f
Yunzhu Chen <chen.yunz@husky.neu.edu>
2018-03-08 11:46:51 -0800

    copy2

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -1,72 +1,103 @@
 <template>
   <v-container fluid>
+        <!--<div class="hello" v-bind:style="{ backgroundImage: bgImage, height: imageHeight }">-->
       <div class="hello">
             <v-layout column align-center>
               <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5" style="width:100px;height:100px;">
               <v-flex>
                 <h1>{{ msg }}</h1>
               </v-flex>
-              <v-flex><li></li></v-flex>
             </v-layout>
 
-            <v-layout column align-center>
-              <v-flex>
-                <li>Zipcode</li>
-                <li>
-                    <input type="search" result = "s" placeholder="Please enter a zip code"
-                           v-model="zipcodeMessage"
-                           v-bind:style="{ height: searchHeight, width: searchWidth, border: '2px lightgrey solid'}">
-                </li>
-              </v-flex>
-              <v-flex><li></li></v-flex>
-              <v-flex>
-                <li>Address</li>
-                <li>
-                    <input type="search" result = "s" placeholder="Please enter an address"
-                           v-model="addressMessage"
-                           v-bind:style="{ height: searchHeight, width: searchWidth, border: '2px lightgrey solid'}">
-                </li>
-              </v-flex>
-              <v-flex><li></li></v-flex>
-              <v-flex>
-                <li>BlockID</li>
-                <li>
-                  <input type="search" result = "s" placeholder="Please enter a block id"
-                         v-model="blockMessage"
-                         v-bind:style="{ height: searchHeight, width: searchWidth, border: '2px lightgrey solid'}">
-                </li>
-              </v-flex>
-            </v-layout>
-
-
-          <v-layout column text-xs-center>
-            <v-flex><li></li></v-flex>
-            <v-flex>
-              <v-btn small color="searchBottonColor" v-on:click="searchProperty">Search</v-btn>
-            </v-flex>
-          </v-layout>
-
-            <v-layout row text-xs-center>
-              <v-flex>
-              <p>{{ searchResult }}</p>
-              <li v-for="(item, key) in singleFamily">
-                {{ item }} {{key}}
-              </li>
-              <li v-for="(item, key) in multiFamily">
-                {{ item }} {{key}}
-              </li>
-              <li v-for="(item, key) in hist">
-                {{ item }} {{key}}
-              </li>
-              </v-flex>
-              <v-flex>
-              </v-flex>
-            </v-layout>
-        <v-layout fill-height>
+        <v-layout column align-center>
+          <v-flex>
+            <li>Zipcode</li>
+            <li>
+              <input type="search"
+                     result = "s"
+                     placeholder="Please enter a zip code"
+                     v-on:keyup.enter="searchZipcode"
+                     v-model.trim="zipcodeMessage"
+                     v-bind:style="{ height: searchHeight, width: searchWidth, border: '2px lightgrey solid'}">
+            </li>
+          </v-flex>
+          <v-flex><li></li></v-flex>
+          <v-flex>
+            <li>Address</li>
+            <li>
+              <input type="search"
+                     result = "s"
+                     placeholder="Please enter an address"
+                     v-on:keyup.enter="searchZipcode"
+                     v-model.trim="addressMessage"
+                     v-bind:style="{ height: searchHeight, width: searchWidth, border: '2px lightgrey solid'}">
+            </li>
+          </v-flex>
+          <v-flex><li></li></v-flex>
+          <v-flex>
+            <li>BlockID</li>
+            <li>
+              <input type="search"
+                     result = "s"
+                     placeholder="Please enter a block id"
+                     v-on:keyup.enter="searchZipcode"
+                     v-model.trim="blockMessage"
+                     v-bind:style="{ height: searchHeight, width: searchWidth, border: '2px lightgrey solid'}">
+            </li>
+          </v-flex>
         </v-layout>
-            <v-layout row text-xs-center>
-              <v-flex>
-              <h2>Reference</h2>
+
+
+        <v-layout column text-xs-center>
+          <v-flex><li></li></v-flex>
+          <v-flex>
+            <v-btn small color="searchBottonColor" v-on:click="searchZipcode">Search</v-btn>
+          </v-flex>
+        </v-layout>
+
+            <v-layout column align-center xs>
+              <div class="hello" v-bind:style="{ backgroundColor: '#D4E8F9', height: 350 + 'px', width: 700 + 'px', overflow: 'auto' }">
+              <div v-if="this.addressMessage !== ''">
+                <v-flex>
+                  <p> Result for address searching </p>
+                </v-flex>
+                <v-flex>
+                  <li v-for="(item, key) in searchResult">
+                    {{key}} {{ item }}
+                  </li>
+                </v-flex>
+              </div>
+              <div v-else-if="this.zipcodeMessage !== ''">
+                <v-flex>
+                <p> Result for zipcode searching </p>
+                </v-flex>
+                <v-flex>
+                  <pre>
+                <li v-for="(item, key) in searchResult">
+                  {{key}} {{ item }}
+                </li>
+                  </pre>
+                </v-flex>
+              </div>
+              <div v-else-if="this.blockMessage !== ''">
+                <v-flex>
+                  <p> Result for block searching </p>
+                </v-flex>
+                <v-flex>
+                  <li v-for="(item, key) in searchResult">
+                    {{key}} {{ item }}
+                  </li>
+                </v-flex>
+              </div>
+              <div v-else>
+                No Information is given
+              </div>
+              </div>
+            </v-layout>
+
+        <v-layout row text-xs-center>
+          <v-flex>
+            <h2>Reference</h2>
             <ul>
               <li><a href="https://www.housecanary.com/" target="_blank">House Canary</a></li>
               <li><a href="https://vuejs.org/">Vue.js</a></li>
@@ -74,16 +105,15 @@
               <br>
               <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for Vue Webpack</a></li>
             </ul>
-              </v-flex>
-              <v-flex>
+          </v-flex>
+          <v-flex>
             <h2>Contacts</h2>
             <ul>
               <li><a href="mailto: chen.yunz@husky.neu.edu" target="_blank">E-mail</a></li>
               <li>(1)781-539-9595</li>
             </ul>
-              </v-flex>
-            </v-layout>
-          <!--</v-slide-y-transition>-->
+          </v-flex>
+        </v-layout>
         </div>
   </v-container>
 </template>
@@ -97,14 +127,14 @@
       return {
         msg: 'Search for a Property!',
         // bgImage: 'url(' + require('../assets/Spain.jpeg') + ')',
-        imageHeight: window.innerHeight + 'px',
+        // imageHeight: window.innerHeight + 'px',
         searchHeight: 40 + 'px',
         searchWidth: 200 + 'px',
         zipcodeMessage: '',
         addressMessage: '',
         blockMessage: '',
         searchBottomColor: '#509088',
-        listColor: '#509088',
+        listColor: '#5892f1',
         searchResult: [],
         singleFamily: [],
         multiFamily: [],
@@ -112,24 +142,22 @@
       }
     },
     methods: {
-      searchProperty(event) {
+      searchZipcode(event) {
         if (event) {
-
-          // var endpointURL = ''
-          // this.addressMessage.replace(/ /g, '+')
-          // if (this.zipcodeMessage && this.addressMessage) {
-          //   endpointURL = '/property/details?address=' + this.addressMessage +
-          //   '&zipcode=' + this.zipcodeMessage
-          // } else if (this.addressMessage) {
-          //   endpointURL = '/property/details?address=' + this.addressMessage
-          // } else if (this.zipcodeMessage) {
-          //   endpointURL = '/zip/details?zipcode=' + this.zipcodeMessage
-          // } else if (this.blockMessage) {
-          //   endpointURL = '/block/value_distribution?block_id=' + this.blockMessage
-          // } else {
-          //   console.log('No information is given.')
-          // }
-          var endpointURL = '/zip/details?zipcode=' + this.zipcodeMessage
+          var endpointURL = ''
+          this.addressMessage.replace(/ /g, '+')
+          if (this.zipcodeMessage !== '' && this.addressMessage !== '') {
+            endpointURL = '/property/details?address=' + this.addressMessage +
+            '&zipcode=' + this.zipcodeMessage
+          } else if (this.addressMessage !== '') {
+            endpointURL = '/property/details?address=' + this.addressMessage
+          } else if (this.zipcodeMessage !== '') {
+            endpointURL = '/zip/details?zipcode=' + this.zipcodeMessage
+          } else if (this.blockMessage !== '') {
+            endpointURL = '/block/value_distribution?block_id=' + this.blockMessage
+          } else {
+            console.log('No information is given.')
+          }
           axios({
             method: 'GET',
             url: '/v2' + endpointURL,
@@ -142,14 +170,14 @@
           })
             .then(response => {
               this.searchResult = response.data[0]
-              // console.log('results data', this.searchResult)
+              console.log('results data', this.searchResult)
               this.singleFamily = this.searchResult['zip/details']['result']['single_family']
               this.multiFamily = this.searchResult['zip/details']['result']['multi_family']
               this.hist = this.searchResult['zip/details']['result']['historical']
             })
-          // .catch(function(error) {
-          //   console.log(error)
-          // })
+              .catch(function(error) {
+                console.log(error)
+              })
         }
       }
     }
